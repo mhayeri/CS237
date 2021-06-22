96090df123ce79c1f4dc06db2f1846d042004865
Mustafa Hayeri <mustafahayeri@yahoo.com>
2021-06-22 13:35:40 -0700

    clean up prog 2 formatting

--- prog2.s
+++ prog2.s
@@ -5,7 +5,7 @@
 * Filename: prog2.s
 * Date completed:  10/27/16
 *---------------------------------------------------------------
-* Problem statement: Find the remainder of the equation using 	      * given values 
+* Problem statement: Find the remainder of the equation using given values 
 * Input: None
 * Output: The answer(remainder) of the given equation 
 * Error conditions tested: None
@@ -15,8 +15,8 @@
 *---------------------------------------------------------------
 *
        ORG     $0
-       DC.L    $3000           * Stack pointer value after a 		  reset
-       DC.L    start           * Program counter value after      	  a reset
+       DC.L    $3000           * Stack pointer value after a reset
+       DC.L    start           * Program counter value after a reset
        ORG     $3000           * Start at location 3000 Hex
 *
 *---------------------------------------------------------------
@@ -30,88 +30,88 @@
 *
 *---------------------------------------------------------------
 *
-DATA	EQU	    $6000
+DATA	    EQU	    $6000
 a:	    EQU	    DATA
 b: 	    EQU	    a+2
 c:	    EQU	    b+2
-d:	    EQU 	c+2
+d:	    EQU     c+2
 e:	    EQU	    d+2
 f:	    EQU	    e+2
 X:	    EQU	    f+2
 Y:	    EQU	    X+2
 Z:	    EQU	    Y+2
 
-start:  initIO          * Initialize (required for I/O)
+start:      initIO            		* Initialize (required for I/O)
 	    setEVT			* Error handling routines
 *	    initF			* For floating point macros only
 
 	    lineout		title
 	    lineout		skipln
-        move.w		X,D1
-        muls.w		D1,D1		     *ax^3
-        muls.w		X,D1
-        muls.w		a,D1
+	    move.w		X,D1
+	    muls.w		D1,D1		     *ax^3
+	    muls.w		X,D1
+	    muls.w		a,D1
 
-        move.w		b,D2
-        asl.w		#1,D2
-        muls.w		Y,D2		     *2by^3	
-        muls.w		Y,D2
-        muls.w		Y,D2
-        add.w		D2,D1
+	    move.w		b,D2
+	    asl.w		#1,D2
+	    muls.w		Y,D2		     *2by^3	
+	    muls.w		Y,D2
+	    muls.w		Y,D2
+	    add.w		D2,D1
         
-        move.w		c,D2
-        muls.w		Z,D2
-        muls.w		Z,D2		      *cz^2
-        add.w		D2,D1
+            move.w		c,D2
+            muls.w		Z,D2
+            muls.w		Z,D2		      *cz^2
+            add.w		D2,D1
 
-        move.w		d,D2
-        muls.w		X,D2
-        muls.w		X,D2		      *dx^2y
-        muls.w		Y,D2
-        sub.w		D2,D1
+            move.w		d,D2
+            muls.w		X,D2
+            muls.w		X,D2		      *dx^2y
+            muls.w		Y,D2
+            sub.w		D2,D1
 
-        move.w		d,D2
-        muls.w		X,D2		      *dx^2
-        muls.w		X,D2
+            move.w		d,D2
+            muls.w		X,D2		      *dx^2
+            muls.w		X,D2
 
-        move.w		e,D3
-        muls.w		Y,D3
-        muls.w		Y,D3		      *ey^2
-        add.w		D3,D2
+            move.w		e,D3
+            muls.w		Y,D3
+            muls.w		Y,D3		      *ey^2
+            add.w		D3,D2
         
-        move.w		f,D3
-        muls.w		X,D3		      *fxb
-        muls.w		b,D3
+            move.w		f,D3
+            muls.w		X,D3		      *fxb
+            muls.w		b,D3
 
-        add.w		D3,D2
-        move.w		Z,D3
-        muls.w		Z,D3		      *3Z^2
-        muls.w		#3,D3
+            add.w		D3,D2
+            move.w		Z,D3
+            muls.w		Z,D3		      *3Z^2
+            muls.w		#3,D3
         
-        move.w		a,D4
-        muls.w		d,D4		      *2ad
-        asl.w		#1,D4
-        sub.w		D4,D3
+            move.w		a,D4
+            muls.w		d,D4		      *2ad
+            asl.w		#1,D4
+            sub.w		D4,D3
         
-        divs.w		D2,D1
-        add.w		D3,D1
-        move.w		D1,D0
+            divs.w		D2,D1
+            add.w		D3,D1
+            move.w		D1,D0
 
-        ext.l		D0
-        divs.w		#100,D0
-        swap		D0
+            ext.l		D0
+            divs.w		#100,D0
+            swap		D0
 
-        ext.l		D0
+            ext.l		D0
 
-        cvt2a		answer,#6
-        stripp		answer,#6
-        lea		    answer,A0
-        adda.l		D0,A0
-        clr.b		(A0)
+            cvt2a		answer,#6
+            stripp		answer,#6
+            lea		        answer,A0
+            adda.l		D0,A0
+            clr.b		(A0)
 
-        lineout		output
+            lineout		output
 
-        break       * Terminate execution
+            break       * Terminate execution
 
 *
 *---------------------------------------------------------------
