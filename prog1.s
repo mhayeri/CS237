6258e50a1396ef7deaec677efad2bf83335fa2d4
Mustafa Hayeri <mustafahayeri@yahoo.com>
2021-06-22 13:31:53 -0700

    clean up prog 1 formatting

--- prog1.s
+++ prog1.s
@@ -30,23 +30,23 @@
 *
 *---------------------------------------------------------------
 *
-start:   initIO           * Initialize (required for I/O)
-	   	 setEVT			  * Error handling routines
-*	   	 initF			  * For floating point macros only	
+start:   	 initIO           	  	 * Initialize (required for I/O)
+	   	 setEVT			  	 * Error handling routines
+*	   	 initF			  	 * For floating point macros only	
 
 		lineout	   title
 		lineout	   skipln
 		lineout    prompt
 		linein	   buffer	         * User input is stored at this address
-		cvta2	   buffer+6,#4	     * Takes 4 bytes starting at address(year * of birth) and coverts to 2s-comp
+		cvta2	   buffer+6,#4	         * Takes 4 bytes starting at address(year * of birth) and coverts to 2s-comp
 		move.l	   D0,D1
 		move.l	   #2016,D0
-		sub.l	   D1,D0		     * Subtracts year of birth from 2016
+		sub.l	   D1,D0		 * Subtracts year of birth from 2016
 		cvt2a	   age,#3
 		stripp     age,#3
-		lea		   age,A0
+		lea        age,A0
 		adda.l	   D0,A0
-		move.b	   #' ',(A0)+	     * Increments address register to complete string
+		move.b	   #' ',(A0)+	         * Increments address register to complete string
 		move.b	   #'y',(A0)+
 		move.b	   #'e',(A0)+
 		move.b	   #'a',(A0)+ 
@@ -59,25 +59,25 @@ start:   initIO           * Initialize (required for I/O)
 		move.b	   #'.',(A0)+
 		move.b	   #' ',(A0)+
 		move.b	   #'*',(A0)+
-		clr.b	   (A0)			     * Null terminate the string
-		lea		   stars+35,A1
+		clr.b	   (A0)			* Null terminate the string
+		lea        stars+35,A1
 		adda.l	   D0,A1
 		clr.b	   (A1)
 		lineout	   stars
 		lineout	   answer
 		lineout	   stars
-		move.b	   #’*',(A1)         * Replace the '*' that gets null terminated		
+		move.b	   #’*',(A1)            * Replace the '*' that gets null terminated		
 
-		break                        * Terminate execution
+		break                           * Terminate execution
 
 *
 *----------------------------------------------------------------------
 *       Storage declarations
-title:	    dc.b	'Program #1, Mustafa Hayeri, cssc0685',0
+title:	    	dc.b	'Program #1, Mustafa Hayeri, cssc0685',0
 prompt: 	dc.b	'Please enter your date of birth (MM/DD/YYYY):',0
 buffer: 	ds.b	80
-answer:	    dc.b	'* In 2016 you will be '
+answer:	    	dc.b	'* In 2016 you will be '
 age:		ds.b	20
-stars:	    dcb.b	40,'*'
-skipln:	dc.b	0	
+stars:	    	dcb.b	40,'*'
+skipln:		dc.b	0	
 	end
